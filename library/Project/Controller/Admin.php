ee7266da0a4151d02742c0d57b7db4b387bff847
Pavel Perminov <pavel.perminov.23@gmail.com>
2013-09-02 15:54:02 +0300

    перенес ссылку Выход в ядро

--- library/Project/Controller/Admin.php
+++ library/Project/Controller/Admin.php
@@ -4,8 +4,4 @@ class Project_Controller_Admin extends Indi_Controller_Admin{
         if ($_SESSION['admin']['id'] == 15 && $this->controller != 'client') unset($_SESSION['admin']);
         parent::preDispatch();
     }
-    public function assign(){
-        parent::assign();
-        $this->view->assign('admin', $this->admin['title'] . ' [' . $this->admin['profile']  . '] <a href="' . $_SERVER['STD'] . '/admin/logout/">Выход</a>');
-    }
 }
