9b0ec01b10a0139e18aba5c00ab53bd5e1302334
Pavel Perminov <vmikhalko@dolphin.timeweb.ru>
2013-12-05 03:08:47 +0400

    forgot to add file

---
+++ application/controllers/WeekController.php
@@ -0,0 +1,21 @@
+<?php
+class WeekController extends Indi_Controller_Front{
+    public function indexAction(){
+        $thisWeekEvents = $this->db->query('
+            SELECT
+                `birthChildName`,
+                `birthChildAge`
+            FROM
+                `event`
+            WHERE 1
+                AND `date` BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 7-(WEEKDAY(CURDATE())+1) DAY)
+                AND LENGTH(`birthChildName`) > 0
+            ORDER BY `date`;
+        ')->fetchAll();
+        
+        for ($i = 0; $i < $thisWeekEvents[$i]; $i++) 
+            $thisWeekEvents[$i]['birthChildAge'] = Misc::tbq($thisWeekEvents[$i]['birthChildAge'], 'лет,год,года');
+
+        die(json_encode($thisWeekEvents));
+    }
+}
