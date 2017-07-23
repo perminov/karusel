4a40358743ba405a789f1724298f0a8fef97e931
Pavel Perminov <pavel.perminov.23@gmail.com>
2017-07-23 13:09:07 +0300

    --

--- application/models/Event/Row.php
+++ application/models/Event/Row.php
@@ -1,34 +1,95 @@
 <?php
 class Event_Row extends Indi_Db_Table_Row_Schedule {
 
-    public function validate() {
+    public function schedule($data = array()) {
+
+        // Check given data against validation rules
+        $this->mcheck(array(
+            'placeId' => array(
+                'req' => true,
+                'rex' => 'int11',
+                'key' => true
+            ),
+            array_key_exists('since', $data) ? 'since,until' : 'date' => array(
+                'req' => true,
+                'rex' => 'date'
+            )
+        ), $data);
 
         // Create schedule
-        $schedule = Indi::schedule('month', $this->date)
-            ->daily('10:00:00', '20:00:00')
-            ->load('event', array(
-                '`placeId` = "' . $this->placeId . '"',
-                '`id` != "' . $this->id . '"',
-                '`manageStatus` != "036#ff9900"'
-            ));
+        $schedule = array_key_exists('since', $data)
+            ? Indi::schedule($this->since, $this->until)
+            : Indi::schedule('month', $this->date);
+
+        // Set daily working hours and load existing events
+        $schedule->daily('10:00:00', '20:00:00')->load('event', array(
+            '`placeId` = "' . $this->placeId . '"',
+            '`id` != "' . $this->id . '"',
+            '`manageStatus` != "036#ff9900"'
+        ));
+
+        // Return $schedule
+        return $schedule;
+    }
+
+    public function busyDates($data = array()) {
+
+        // Get schedule
+        $schedule = $this->schedule($data);
 
         // Get desired space frame
         $frame = $this->foreign('placeId')->duration . 'm';
 
         // Get busy dates
-        $busyDateA = $schedule->busyDates($frame);
+        $busyA = $schedule->busyDates($frame);
 
-        // If current `date` is a totally busy date - set mismatch message
-        if (in($this->date, $busyDateA)) $this->mflush('date', 'Эта дата полностью занята');
+        // If $data arg was given - just return busy dates
+        if (func_num_args()) return $busyA;
+
+        // Else if current `date` is a totally busy date - set mismatch message
+        else if (in($this->date, $busyA)) $this->mflush('date', 'Эта дата полностью занята');
+    }
+
+    public function busyTimes($data = array()) {
+
+        // Get schedule
+        $schedule = $this->schedule($data);
+
+        // Get desired space frame
+        $frame = $this->foreign('placeId')->duration . 'm';
 
         // Get busy hours
-        $hourA = $schedule->busyHours($frame, $this->date, '30m');
+        $busyA = $schedule->busyHours($frame, $this->date, '30m');
 
-        // Get disabled ids
-        $busyTimeIdA = Indi::model('Time')->fetchAll('FIND_IN_SET(`title`, "' . im($hourA) . '")')->column('id');
+        // Get list of invalid values of timeId` prop
+        $busyTimeIdA = Indi::model('Time')->fetchAll('FIND_IN_SET(`title`, "' . im($busyA) . '")')->column('id');
 
-        // If current `timeId` is a totally busy time - set mismatch message
-        if (in($this->timeId, $busyTimeIdA)) $this->mflush('time', 'Это время занято');
+        // If $data arg was given - just return busy time ids
+        if (func_num_args()) return $busyTimeIdA;
+
+        // Else if current `timeId` is a totally busy time - set mismatch message
+        if (in($this->timeId, $busyTimeIdA)) $this->mflush('timeId', 'Это время занято');
+    }
+
+    public function busyAnimators($data = array()) {
+
+        // Check $data arg
+        $this->mcheck(array(
+            'placeId' => array(
+                'req' => true,
+                'rex' => 'int11',
+                'key' => 'Place'
+            ),
+            'date' => array(
+                'req' => true,
+                'rex' => 'date'
+            ),
+            'timeId' => array(
+                'req' => true,
+                'rex' => 'int11',
+                'key' => 'Time'
+            )
+        ), $data);
 
         // General WHERE clause
         $where = array(
@@ -38,38 +99,77 @@ class Event_Row extends Indi_Db_Table_Row_Schedule {
         );
 
         // Gap, in seconds
-        $gap = 1800;
+        Indi::registry('gap', 1800);
+
+        // If $data arg is given
+        if (func_num_args()) {
+
+            // Append additional clause
+            $where[] = '`date` = "' . $this->date . '" AND `animatorId` != ""';
+
+            // Get animators, involved in events at date, specified by $data['date']
+            $animatorIdA = array_unique(ar($this->model()->fetchAll($where)->column('animatorId', true, true)));
+
+            // Remove additional clause
+            array_pop($where);
+
+        // Else use current value of `animatorId` prop
+        } else $animatorIdA = $this->zero('animatorId') ? array() : ar($this->animatorId);
 
         // If `animatorId` prop not yet set
         // Foreach animator, involved in events at date, specified by $data['date']
-        $busyAnimatorA = array(); if ($this->animatorId) foreach (ar($this->animatorId) as $animatorId) {
+        $busyA = array(); foreach ($animatorIdA as $animatorId) {
+
+            // Append animator-related clause
+            $where[] = 'FIND_IN_SET("' . $animatorId . '", `animatorId`)';
 
             // Create schedule, set daily active hours and load animator's events
             $schedule = Indi::schedule('week', $this->date)
-                ->daily('10:00:00', '20:30:00')
-                ->load('event', array_merge(array('FIND_IN_SET("' . $animatorId . '", `animatorId`)'), $where), function(&$r, $sp) {
-                    $r->{$sp['frame']} += ($gap = 1800);
+                ->daily('10:00:00', '20:' . (Indi::registry('gap')/60) . ':00')
+                ->load('event', $where, function(&$r, $sp) {
+                    $r->{$sp['frame']} += Indi::registry('gap');
                 });
+
+            // Remove animator-related clause
+            array_pop($where);
 
             // If animator is busy - push it's id to $busyAnimatorA array
             if ($schedule->busy(
                 $this->date . ' ' . $this->foreign('timeId')->title . ':00',
-                $this->foreign('placeId')->duration * 60 + $gap,
-                true
-            )) $busyAnimatorA[] = $animatorId;
+                $this->foreign('placeId')->duration * 60 + Indi::registry('gap'), true
+            )) $busyA[] = $animatorId;
         }
 
-        // If one or more of selected animators are busy - flush error message
-        if (count($busyAnimatorA)) $this->mflush('animatorId', 'Уже занятые аниматоры: '
-            . $this->foreign('animatorId')->select($busyAnimatorA)->column('title', ', '));
+        // If $data arg was given - just return busy time ids
+        if (func_num_args()) return $busyA;
 
-        $this->mflush('asd', 'asd');
+        // If one or more of selected animators are busy - flush error message
+        else if (count($busyA)) $this->mflush('animatorId', 'Уже занятые аниматоры: '
+            . $this->foreign('animatorId')->select($busyA)->column('title', ', '));
+    }
+
+    public function validate() {
+
+        // Check that event's date is not busy
+        $this->busyDates();
+
+        // Check that event's time is not busy
+        $this->busyTimes();
+
+        // If `programId` is non-zero - check that event's animators are not busy
+        if (!$this->zero('programId')) $this->busyAnimators();
+
+        // Call parent
+        return $this->callParent();
     }
 
     /**
      * @return int|mixed
      */
     public function save() {
+
+        // Check types
+        $this->scratchy(true);
 
         // Set `title`
         $this->title = sprintf('[%s, %s] %s: %s', $this->date, $this->foreign('timeId')->title,
@@ -79,11 +179,29 @@ class Event_Row extends Indi_Db_Table_Row_Schedule {
         if ($this->isModified('birthChildBirthDate')) $this->birthChildAge
             = $this->zero('birthChildBirthDate') ? 0 : date('Y') - $this->date('birthChildBirthDate', 'Y');
 
+        // Set price
+        $this->price();
+
+        // Call parent
+        return parent::save();
+    }
+
+    public function price() {
+
+        // Check
+        $this->mcheck(array(
+            'programId' => array('rex' => 'int11', 'key' => true),
+            'subprogramId' => array('rex' => 'int11', 'key' => true)
+        ));
+
         // Animators qty
         $aQty = $this->foreign('subprogramId')->animatorsCount ?: 1;
 
+        // Set `districtId`
+        if (!$this->districtId) $this->districtId = $this->foreign('placeId')->districtId;
+
         // Get initial price
-        $this->price = $this->foreign('placeId')->foreign('districtId')->{'price' . $aQty};
+        $this->price = $this->foreign('districtId')->{'price' . $aQty};
 
         // If event date is not a saturday/sunday and is not an other holiday
         if (!in(date('N', strtotime($this->date)), '6,7')
@@ -95,8 +213,8 @@ class Event_Row extends Indi_Db_Table_Row_Schedule {
             $this->price *= 1 - $discount/100;
         }
 
-        // Call parent
-        return parent::save();
+        // Return price
+        return $this->price;
     }
 
     /**
--- library/Project/Controller/Admin/Events.php
+++ library/Project/Controller/Admin/Events.php
@@ -34,159 +34,20 @@ class Project_Controller_Admin_Events extends Project_Controller_Admin {
 
     public function formActionIDate($data) {
 
-        // Check $data arg
-        $rowA = jcheck(array(
-            'placeId' => array(
-                'req' => true,
-                'rex' => 'int11',
-                'key' => 'Place'
-            ),
-            'since,until' => array(
-                'req' => true,
-                'rex' => 'date'
-            )
-        ), $data);
-
-        // Get desired space frame
-        $frame = $rowA['placeId']->duration . 'm';
-
-        // Create schedule
-        $schedule = Indi::schedule($data['since'], $data['until'])
-            ->daily('10:00:00', '20:00:00')
-            ->load('event', array(
-                '`placeId` = "' . $data['placeId'] . '"',
-                '`id` != "' . $this->row->id . '"',
-                '`manageStatus` != "036#ff9900"'
-            ));
-
         // Flush disabled dates
-        jflush(true, array('disabledDates' => $schedule->busyDates($frame)));
+        jflush(true, array('disabledDates' => $this->row->busyDates($data)));
     }
 
     public function formActionITimeId($data) {
 
-        // Check $data arg
-        $rowA = jcheck(array(
-            'placeId' => array(
-                'req' => true,
-                'rex' => 'int11',
-                'key' => 'Place'
-            ),
-            'date' => array(
-                'req' => true,
-                'rex' => 'date'
-            )
-        ), $data);
-
-        // Create schedule
-        $schedule = Indi::schedule('week', $data['date'])
-            ->daily('10:00:00', '20:00:00')
-            ->load('event', array(
-                '`placeId` = "' . $data['placeId'] . '"',
-                '`id` != "' . $this->row->id . '"',
-                '`manageStatus` != "036#ff9900"'
-            ));
-
-        // Get desired space frame
-        $frame = $rowA['placeId']->duration . 'm';
-
-        // Check that startDate is not busy
-        if (in($data['date'], $schedule->busyDates($frame))) jflush(false, 'Выберите другую дату');
-
-        // Get busy hours
-        $hourA = $schedule->busyHours($frame, $data['date'], '30m');
-
-        // Get disabled ids
-        $timeIdA = Indi::model('Time')->fetchAll('FIND_IN_SET(`title`, "' . im($hourA) . '")')->column('id');
-
         // Flush busy time ids
-        jflush(true, array('disabledTimeIds' => $timeIdA));
+        jflush(true, array('disabledTimeIds' => $this->row->busyTimes($data)));
     }
 
     public function formActionIAnimatorId($data) {
 
-        // Check $data arg
-        $rowA = jcheck(array(
-            'placeId' => array(
-                'req' => true,
-                'rex' => 'int11',
-                'key' => 'Place'
-            ),
-            'timeId' => array(
-                'req' => true,
-                'rex' => 'int11',
-                'key' => 'Time'
-            ),
-            'date' => array(
-                'req' => true,
-                'rex' => 'date'
-            ),
-            'programId' => array(
-                'req' => true,
-                'rex' => 'int11',
-                'key' => 'Program'
-            ),
-            'subprogramId' => array(
-                'req' => false,
-                'rex' => 'int11',
-                'key' => 'Subprogram'
-            )
-        ), $data);
-
-        // Declare array for disabled animator ids
-        $disabledA = array();
-
-        // General WHERE clause
-        $where = array(
-            '`manageStatus` != "036#ff9900"',
-            '`placeId` != "' . $data['placeId'] . '"',
-            '`id` != "' . $this->row->id . '"',
-        );
-
-        // Gap, in seconds
-        $gap = 1800;
-
-        // Get animators, involved in events at date, specified by $data['date']
-        $animatorIdA = array_unique(ar(Indi::model('Event')
-            ->fetchAll(array_merge($where, array('`date` = "' . $data['date'] . '"', '`animatorId` != ""')))
-            ->column('animatorId', true, true)));
-
-        // Foreach animator, involved in events at date, specified by $data['date']
-        foreach ($animatorIdA as $animatorId) {
-
-            // Create schedule
-            $schedule = Indi::schedule('week', $data['date'])
-
-                // Set daily active hours
-                ->daily('10:00:00', '20:30:00')
-
-                // Load animator's events
-                ->load('event', array_merge(array('FIND_IN_SET("' . $animatorId . '", `animatorId`)'), $where), function(&$r, $sp) {
-                    $r->{$sp['frame']} += ($gap = 1800);
-                });
-
-            // If animator is busy - push it's id to $disabled array
-            if ($schedule->busy($data['date'] . ' ' . $rowA['timeId']->title . ':00', $rowA['placeId']->duration * 60 + $gap, true)) $disabledA[] = $animatorId;
-        }
-
-        // Animators qty
-        $aQty = $rowA['subprogramId']->animatorsCount ?: 1;
-
-        // Get initial price
-        $price = $rowA['placeId']->foreign('districtId')->{'price' . $aQty};
-
-        // If $data['date'] is not a saturday/sunday and is not a holiday other holiday
-        if (!in(date('N', strtotime($data['date'])), '6,7')
-            && !Indi::model('Holiday')->fetchRow('`title` = "' . $data['date'] . '"')
-            && ($discount = Indi::blocks('work-day-discount'))
-            && ((!$until = Indi::blocks('discount-until-time')) || $rowA['timeId']->title < $until)) {
-
-            // Get discounted price
-            $price *= 1 - $discount/100;
-        }
-
         // Flush busy time ids
-        jflush(true, array('disabled' => $disabledA, 'price' => $price));
+        jflush(true, array('disabled' => $this->row->busyAnimators($data), 'price' => $this->row->price()));
     }
 
     /**
